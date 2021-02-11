c81f631807efa188d4ac110020f282c945aea60d
Harsha Priyank Nerella <nerellapriyank@gmail.com>
2021-02-11 22:30:03 +0530

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Harsha Priyank, Nerella V on 11/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
